6e88053aa8e8406e0d6f3d7fd2f8ce30a6f15ca4
Billy <chrysophylax@users.noreply.github.com>
2016-11-16 19:43:22 +0100

    Test från läraren

---
+++ testInheritance.js
@@ -0,0 +1,10 @@
+//Beatrice's test for prototype-based inheritance
+
+var obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+var obj1 = myObject.create([obj0]);
+var obj2 = myObject.create([]);
+obj2.func = function(arg) { return "func2: " + arg; };
+var obj3 = myObject.create([obj1, obj2]);
+var result = obj3.call("func", ["hello"]);
+console.log("result: " + result);
