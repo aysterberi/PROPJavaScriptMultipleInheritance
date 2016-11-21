2f92278ff5e791fe820d08a5edeb4d6ca7bba601
Joakim Berglund <joakimberglund@live.se>
2016-11-21 09:53:59 +0100

    begun writing ClassBased.js, based on other repositories i have take the wrong approach

--- ClassBased.js
+++ ClassBased.js
@@ -1,16 +1,39 @@
-function createClass (className, superClassList) {
+function createClass(className, superClassList) {
+    this.className = className;
+    this.superClassList = superClassList;
 
 }
-createClass.new = function () {
 
+createClass.new = function () {
+    var instanceObject = {
+        call: function (funcName, parameters) {
+            if (funcName in this)
+                return this[funcName].apply(null, parameters);
+            else if (this.prototypeList != null)
+                for (var i = 0; i < this.prototypeList.length; i++) {
+                    if ("call" in this.prototypeList[i]) {
+                        var found = this.prototypeList[i].call(funcName, parameters);
+                        if (found != null)
+                            return found;
+                    }
+                }
+            return null;
+        }
+    }
 };
 
+
 /* Test from Beatrice, should output: "func0: hello" */
-const class0 = createClass("Class0", null);
-class0.func = function(arg) { return "func0: " + arg; };
-const class1 = createClass("Class1", [class0]);
-const class2 = createClass("Class2", []);
-class2.func = function(arg) { return "func2: " + arg; };
-const class3 = createClass("Class3", [class1, class2]);
-const obj3 = class3.new();
-const result = obj3.call("func", ["hello"]);
+var class0 = createClass("Class0", null);
+class0.func = function (arg) {
+    return "func0: " + arg;
+};
+var class1 = createClass("Class1", [class0]);
+var class2 = createClass("Class2", []);
+class2.func = function (arg) {
+    return "func2: " + arg;
+};
+var class3 = createClass("Class3", [class1, class2]);
+var obj3 = class3.new();
+var result = obj3.call("func", ["hello"]);
+document.writeln(result);
