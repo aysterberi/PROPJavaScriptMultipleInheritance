7fa2e12e46d9f11e68ef9914b651ed5b100d3da8
Joakim Berglund <joakimberglund@live.se>
2016-11-28 15:19:00 +0100

    refactor

--- PrototypeBased.js
+++ PrototypeBased.js
@@ -1,23 +1,23 @@
-var myObject = {
-    prototypeList: null,
-    create: function (prototypeList) {
-        var obj = Object.create(myObject);
-        obj.prototypeList = prototypeList;
-        return obj;
-    },
-    call: function (functionName, parameters) {
-        if (this.hasOwnProperty(functionName))
-            return this[functionName].apply(null, parameters);
-        else if (this.prototypeList != null)
-            for (var i = 0; i < this.prototypeList.length; i++) {
-                if ("call" in this.prototypeList[i]) {
-                    var found = this.prototypeList[i].call(functionName, parameters);
-                    if (found != null)
-                        return found;
-                }
+var myObject = { prototypeList: null };
+
+myObject.call = function (functionName, parameters) {
+    if (this.hasOwnProperty(functionName))
+        return this[functionName].apply(null, parameters);
+    else if (this.prototypeList != null)
+        for (var i = 0; i < this.prototypeList.length; i++) {
+            if ("call" in this.prototypeList[i]) {
+                var found = this.prototypeList[i].call(functionName, parameters);
+                if (found != null)
+                    return found;
             }
-        return null;
-    }
+        }
+    return null;
+};
+
+myObject.create = function (prototypeList) {
+    var obj = Object.create(myObject);
+    obj.prototypeList = prototypeList;
+    return obj;
 };
 
 /* Test from Beatrice, should output: "func0: hello" */
@@ -28,5 +28,5 @@ var obj2 = myObject.create([]);
 obj2.func = function(arg) { return "func2: " + arg; };
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
-document.writeln(result);
+//document.writeln(result);
 
