9a000c9c5b2271f5e3e2fe71384cebad1719b571
Billy <chrysophylax@users.noreply.github.com>
2016-11-29 16:06:09 +0100

    Adding tests for prototypebas

--- PrototypeBased.js
+++ PrototypeBased.js
@@ -41,5 +41,58 @@ console.log(obj2.func());
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log(result);
+
+// TESTS
+
+console.log("Running tests…");
+CyclicInheritanceShouldBePrevented = function () {
+    //cyclic inheritance test
+    console.log("\tCyclicInheritanceShouldBePrevented");
+    var testObj = myObject.create(null);
+    testObj.ancestors = [testObj, obj0, obj2];
+    assertEquals(undefined, testObj);
+};
+
+UndefinedFunctionShouldBeUndefined = function () {
+    //undefined test
+    console.log("\tUndefinedFunctionShouldBeUndefined");
+    var testObj = myObject.create(null);
+    assertEquals(undefined, testObj.call("NonexistentFunc", []));
+};
+
+DiamondProblemShouldBePrevented = function () {
+    console.log("\tDiamondProblemShouldBePrevented");
+    var count = 0;
+    var Movable = myObject.create(null);
+    var Aeroplane = myObject.create([Movable]);
+    var LandVehicle = myObject.create([Movable]);
+    var AeroCar = myObject.create([LandVehicle, Aeroplane]); //"extends LandVehicle…"
+    //this function should only be called once
+    Movable.accelerate = function (speed) {
+        count++;
+    };
+    AeroCar.call('accelerate', [90.0]);
+    assertEquals(1, count);
+
+};
+
+runAllTests = function () {
+    DiamondProblemShouldBePrevented();
+    CyclicInheritanceShouldBePrevented();
+    UndefinedFunctionShouldBeUndefined();
+};
+
+runAllTests();
+
+//helper utils
+function assertEquals(expected, actual) {
+    if (expected == actual) {
+        console.log("\t\tOK.");
+        return true;
+    }
+    console.log("\t\tAssertionError:\tExpected '" + expected + "' but got '" + actual + "'.");
+    return false;
+}
+
 document.writeln(result);
 
