0cd21d858d7d7d4ce6395fd75fa857a871636e7f
Joakim Berglund <joakimberglund@live.se>
2016-11-29 12:04:56 +0100

    Progress

--- PrototypeBased.js
+++ PrototypeBased.js
@@ -1,22 +1,32 @@
-var myObject = { prototypeList: null };
+var myObject = {};
+
+myObject.prototypeList = null;
 
 myObject.call = function (functionName, parameters) {
+    // for (var name in this)
+    //     if (name == functionName)
+    //         return this[name].apply(null, parameters);
+
+    /* Kan inte bedöma om den övre och den undre fungerar på samma sätt */
+    /* Det borde vara samma eftersom den övre loopar igenom och kollar
+     * efter funktionsnamnet, medan den undre frågar klassen om den har det
+     * funktionsnamnet bara? */
+
     if (this.hasOwnProperty(functionName))
         return this[functionName].apply(null, parameters);
-    else if (this.prototypeList != null)
-        for (var i = 0; i < this.prototypeList.length; i++) {
-            if ("call" in this.prototypeList[i]) {
-                var found = this.prototypeList[i].call(functionName, parameters);
-                if (found != null)
-                    return found;
-            }
-        }
-    return null;
+
+
+    for (var i = 0; i < this.ancestors.length; i++)
+        return this.ancestors[i].length != 0 ? this.ancestors[i].call(functionName, parameters) : 0;
 };
 
 myObject.create = function (prototypeList) {
-    var obj = Object.create(myObject);
-    obj.prototypeList = prototypeList;
+    var obj = Object.create(this);
+    obj.ancestors = [];
+    if (prototypeList == null)
+        return obj;
+    for (var j = 0; j < prototypeList.length; j++)
+        obj.ancestors[j] = prototypeList[j]
     return obj;
 };
 
