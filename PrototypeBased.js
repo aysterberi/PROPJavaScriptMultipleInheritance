579507b3826cae30ec57629fee439d34235b1771
Joakim Berglund <joakimberglund@live.se>
2016-11-30 14:10:23 +0100

    refactor

--- PrototypeBased.js
+++ PrototypeBased.js
@@ -3,19 +3,8 @@ var myObject = {};
 myObject.prototypeList = null;
 
 myObject.call = function (functionName, parameters) {
-    // TODO: Remove all these comments before submitting
-    // for (var name in this)
-    //     if (name == functionName)
-    //         return this[name].apply(null, parameters);
-
-    /* Kan inte bedöma om den övre och den undre fungerar på samma sätt */
-    /* Det borde vara samma eftersom den övre loopar igenom och kollar
-     * efter funktionsnamnet, medan den undre frågar klassen om den har det
-     * funktionsnamnet bara? */
-
     if (this.hasOwnProperty(functionName))
         return this[functionName].apply(null, parameters);
-
     for (var i = 0; i < this.ancestors.length; i++)
         return this.ancestors[i].length != 0 ? this.ancestors[i].call(functionName, parameters) : null;
 };
@@ -25,18 +14,21 @@ myObject.create = function (prototypeList) {
     obj.ancestors = [];
     if (prototypeList == null)
         return obj;
-    for (var j = 0; j < prototypeList.length; j++)
-        obj.ancestors[j] = prototypeList[j]
+    obj.ancestors = prototypeList;
     return obj;
 };
 
 /* Test from Beatrice, should output: "func0: hello" */
 var obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
+obj0.func = function (arg) {
+    return "func0: " + arg;
+};
 console.log(obj0.func());
 var obj1 = myObject.create([obj0]);
 var obj2 = myObject.create([]);
-obj2.func = function(arg) { return "func2: " + arg; };
+obj2.func = function (arg) {
+    return "func2: " + arg;
+};
 console.log(obj2.func());
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
